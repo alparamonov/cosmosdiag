65f3559c4eabec8e07b39e65c87d287fb3adcc15
alparamonov <alex.s.paramonov@gmail.com>
2020-08-25 14:03:21 +0200

    first commit

---
+++ cosmosdiag/DiagData.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cosmosdiag
+{
+    public class DiagData
+    {
+        public string DiagnosticVersion { get; set; }
+        public Summary Summary { get; set; }
+        public Context[] Context { get; set; }
+    }
+
+    public class Summary
+    {
+        public DateTime StartUtc { get; set; }
+        public float TotalElapsedTimeInMs { get; set; }
+        public string UserAgent { get; set; }
+        public int TotalRequestCount { get; set; }
+        public int FailedRequestCount { get; set; }
+    }
+
+    public class Context
+    {
+        public string Id { get; set; }
+        public float ElapsedTimeInMs { get; set; }
+        public string PKRangeId { get; set; }
+        public DateTime StartUtc { get; set; }
+        public string QueryMetric { get; set; }
+        public string IndexUtilization { get; set; }
+        public string ClientCorrelationId { get; set; }
+        [JsonProperty("Context")]
+        public ContextDetailed[] ContextDetailed { get; set; }
+    }
+
+    public class ContextDetailed
+    {
+        public string Id { get; set; }
+        public object[] ContactedReplicas { get; set; }
+        public string[] RegionsContacted { get; set; }
+        public object[] FailedReplicas { get; set; }
+        public float HandlerElapsedTimeInMs { get; set; }
+        public string CpuHistory { get; set; }
+        public float ElapsedTimeInMs { get; set; }
+        public DateTime StartTimeUtc { get; set; }
+        public DateTime EndTimeUtc { get; set; }
+        public string TargetEndpoint { get; set; }
+        public DateTime ResponseTimeUtc { get; set; }
+        public string ResourceType { get; set; }
+        public string OperationType { get; set; }
+        public string LocationEndpoint { get; set; }
+        public string ActivityId { get; set; }
+        public string StoreResult { get; set; }
+    }
+}
---
+++ cosmosdiag/Program.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.Azure.Documents;
+using System.Linq;
+
+namespace cosmosdiag
+{
+    class Program
+    {
+        private readonly string endpoint = "https://<ENDPOINT>.documents.azure.com:443/";
+        private readonly string key1 = "<SECRET_KEY>";
+        private readonly string databaseId = "<DATABASE_NAME>";
+        private readonly string containerId = "<CONTAINER_NAME>";
+
+        private CosmosClient cosmosClient;
+        private Microsoft.Azure.Cosmos.Database database;
+        private Container container;
+
+        static async Task Main()
+        {
+            Program p = new Program();
+            p.PrepareConnection();
+            await p.RunDiag(10);
+        }
+
+        private void PrepareConnection()
+        {
+            this.cosmosClient = new CosmosClient(
+              endpoint,
+              key1,
+              new CosmosClientOptions()
+              {
+                  ApplicationName = "CosmosDiag",
+                  ApplicationRegion = LocationNames.WestEurope
+              });
+            
+            GetDatabase();
+            GetContainer();
+        }
+
+        private async Task RunDiag(int repetitions)
+        {
+            for (int i = 0; i < repetitions; i++)
+            {
+                await QueryItemsAsync();
+            }
+        }
+
+        private void GetDatabase()
+        {
+            this.database = this.cosmosClient.GetDatabase(databaseId);
+        }
+
+        private void GetContainer()
+        {
+            this.container = this.database.GetContainer(containerId);
+        }
+
+        private async Task QueryItemsAsync()
+        {
+            var query = "SELECT * FROM c WHERE c.id = '123'";
+
+            QueryDefinition queryDefinition = new QueryDefinition(query);
+            FeedIterator<dynamic> queryResultSetIterator =
+                this.container.GetItemQueryIterator<dynamic>(queryDefinition, requestOptions: new QueryRequestOptions()
+                {
+                    PartitionKey = new Microsoft.Azure.Cosmos.PartitionKey("123")
+                });
+
+            List<dynamic> items = new List<dynamic>();
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                Microsoft.Azure.Cosmos.FeedResponse<dynamic> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                string diagDataStr = currentResultSet.Diagnostics.ToString();
+
+                DiagData data = JsonConvert.DeserializeObject<DiagData>(diagDataStr);
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Total Elapsed time:\t{data.Summary.TotalElapsedTimeInMs / 1000} s");
+                sb.AppendLine($"Total Request count:\t{data.Summary.TotalRequestCount}");
+                sb.AppendLine($"Create Query Time:\t{data.Context[0].ElapsedTimeInMs / 1000} s");
+                sb.AppendLine($"Service Interop Time:\t{data.Context[1].ElapsedTimeInMs / 1000} s");
+                sb.AppendLine("Query Metrics:");
+                var queryMetrics = data.Context[2].QueryMetric.Split(';');
+                foreach (string metric in queryMetrics)
+                {
+                    sb.AppendLine($"\t{metric}");
+                }
+                string physicalStore = data
+                    .Context[2]
+                    .ContextDetailed
+                    .Last(a => a.StoreResult != null)
+                    .StoreResult
+                    .Split(' ')[1];
+                sb.AppendLine($"\t{physicalStore}");
+                Console.WriteLine(sb.ToString());
+                foreach (dynamic item in currentResultSet)
+                {
+                    items.Add(item);
+                    Console.WriteLine("\tRead {0}\n", item);
+                }
+            }
+        }
+    }
+}
